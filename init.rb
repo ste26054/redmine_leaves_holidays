5ec138e8a0f9b252026a4442a0c87ece10bd1dcd
ste26054 <ste26054@gmail.com>
2015-04-16 14:08:12 +0100

    init

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_leaves_holidays do
+  name 'Redmine Leaves Holidays plugin'
+  author 'Stephane EVR'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
