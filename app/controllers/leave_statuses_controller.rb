a468a021ba3415471694f9ec1d18f58718c7ff69
ste26054 <ste26054@gmail.com>
2015-04-28 18:10:22 +0100

    Refuse creation of another leave request status if already exists

--- app/controllers/leave_statuses_controller.rb
+++ app/controllers/leave_statuses_controller.rb
@@ -1,6 +1,8 @@
 class LeaveStatusesController < ApplicationController
   unloadable
   include LeavesHolidaysLogic
+  before_action :set_leave_request
+  before_action :set_leave_status
 
 
   def index
@@ -9,14 +11,19 @@ class LeaveStatusesController < ApplicationController
   def new
     Rails.logger.info "IN STATUSES NEW"
     @leave = LeaveRequest.find(params[:leave_request_id])
-    @status = LeaveStatus.new if @status == nil
+    # @status = LeaveStatus.new if @status == nil
+    if @status != nil
+      render_403
+    else
+      @status = LeaveStatus.new
+    end
   end
 
   def create
     @leave = LeaveRequest.find(params[:leave_request_id])
 
     Rails.logger.info "IN STATUSES CREATE"
-    @status = LeaveStatus.new(leave_status_params)
+    @status = LeaveStatus.new(leave_status_params) if @status == nil
     @status.leave_request = @leave
 
     if @status.save
@@ -46,4 +53,18 @@ class LeaveStatusesController < ApplicationController
   def leave_status_params
     params.require(:leave_status).permit(:leave_request_id, :processed_date, :user_id, :comments, :acceptance_status)
   end
+
+  def set_leave_status
+    @status = LeaveStatus.where(leave_request_id: @leave.id).first
+    Rails.logger.info "IN SET LEAVE STATUS: LEAVE ID: #{@leave.id}, STATUS: #{@status}"
+  end
+
+  def set_leave_request
+    @leave = LeaveRequest.where(id: params[:leave_request_id]).first
+    Rails.logger.info "SET LEAVE REQUEST: #{@leave}"
+    if @leave == nil
+      render_403
+    end
+  end
+
 end
