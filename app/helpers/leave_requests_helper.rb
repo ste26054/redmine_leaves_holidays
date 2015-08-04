3a196973f3c48bbf61cfcb763efd6c0ffff0f0c8
ste26054 <ste26054@gmail.com>
2015-08-04 16:50:54 +0100

    remove tab parameter and use controller_name instead

--- app/helpers/leave_requests_helper.rb
+++ app/helpers/leave_requests_helper.rb
@@ -3,10 +3,10 @@ module LeaveRequestsHelper
 
 	def render_leave_tabs(tabs, selected=params[:tab])
 	    if tabs.any?
-	      unless tabs.detect {|tab| tab[:name] == selected}
+	      unless tabs.detect {|tab| tab[:controller] == selected}
 	        selected = nil
 	      end
-	      selected ||= tabs.first[:name]
+	      selected ||= tabs.first[:controller]
 	      render :partial => 'leave_commons/tabs', :locals => {:tabs => tabs, :selected_tab => selected}
 	    else
 	      content_tag 'p', l(:label_no_data), :class => "nodata"
@@ -14,16 +14,15 @@ module LeaveRequestsHelper
 	end
 
 	def leaves_holidays_tabs
-		tabs = [{:name => 'requests', :label => :tab_my_leaves, :controller => 'leave_requests', :action => 'index'}]
+		tabs = []
+		tabs << {:label => :tab_my_leaves, :controller => 'leave_requests', :action => 'index'}
 
 		if LeavesHolidaysLogic::user_has_any_manage_right(@user)
-			tabs.insert 1, {:name => 'approvals', :label => :tab_leaves_approval, :controller => 'leave_approvals', :action => 'index'}
+			tabs << { :label => :tab_leaves_approval, :controller => 'leave_approvals', :action => 'index'}
 		end
-		tabs << {:name => 'calendar', :label => :tab_leaves_calendar, :controller => 'leave_calendars', :action => 'show'}
-		tabs
+
+		tabs << {:label => :tab_leaves_calendar, :controller => 'leave_calendars', :action => 'show'}
+		return tabs
 	end
 
-	def link_to_month(link_name, year, month, options={})
-    	link_to_content_update(h(link_name), params.merge(:year => year, :month => month, :tab => "calendar"), options)
-  	end
 end
