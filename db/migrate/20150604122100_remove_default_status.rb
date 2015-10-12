7effd30795cc9b70cb772991ecbded53897195d4
ste26054 <ste26054@gmail.com>
2015-10-12 10:01:24 +0100

    Fix ActiveRecord::IrreversibleMigration errors

--- db/migrate/20150604122100_remove_default_status.rb
+++ db/migrate/20150604122100_remove_default_status.rb
@@ -1,6 +1,14 @@
 class RemoveDefaultStatus < ActiveRecord::Migration
-  def change
-  		change_column_default(:leave_statuses, :acceptance_status, nil)
+  # def change
+  # 		change_column_default(:leave_statuses, :acceptance_status, nil)
+  # end
+
+  def up
+    change_column :leave_statuses, :acceptance_status, :integer, default: nil
+  end
+
+  def down
+    change_column :leave_statuses, :acceptance_status, :integer, default: 0
   end
 end
 
