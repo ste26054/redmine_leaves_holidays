d7f2093b5bd571a2a8266c77d6ec57dedc6555d4
ste26054 <ste26054@gmail.com>
2015-10-22 10:00:15 +0100

    delete useless migrations

--- db/migrate/20151015161900_create_leave_managements.rb
+++
@@ -1,7 +0,0 @@
-class CreateLeaveManagements < ActiveRecord::Migration
-  def change
-    create_table :leave_managements do |t|
-      
-    end
-  end
-end
--- db/migrate/20151019094600_drop_leave_managements.rb
+++
@@ -1,9 +0,0 @@
-class DropLeaveManagements < ActiveRecord::Migration
-  def up
-    drop_table :leave_managements
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
